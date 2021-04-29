875a90142508738e3c72fa8578e4f7c91ef22970
sagatzt <sagatzt@gmail.com>
2021-04-29 09:04:54 +0200

    ok

--- routes/rtMain.js
+++ routes/rtMain.js
@@ -7,30 +7,5 @@ rtMain.get('/', function (req, res) {
     res.json({response:'ok'})
 })
 
-rtMain.post('/create',(req,res)=>{
-    console.log(req.body)
-    daoUser.create(req.body).then(data=>{
-        res.json(data)
-    })
-})
-
-rtMain.get('/findAll',(req,res)=>{
-    daoUser.findAll().then(data=>{
-        res.json(data)
-    })
-})
-
-rtMain.get('/findByEmail',(req,res)=>{
-    console.log(req.query.email)
-    daoUser.findByEmail(req.query.email).then(data=>{
-        res.json(data)
-    })
-})
-
-rtMain.get('/findAllArticles',(req,res)=>{
-    daoUser.findAllArticles().then(data=>{
-        res.json(data)
-    })
-})
 
 module.exports=rtMain
