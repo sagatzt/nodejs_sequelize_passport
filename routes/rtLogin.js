357cdc401463403d751d521e06f7f35d6cbb06c9
sagatzt <sagatzt@gmail.com>
2021-04-29 09:07:56 +0200

    ok

--- routes/rtLogin.js
+++ routes/rtLogin.js
@@ -1,12 +1,8 @@
 const express=require('express')
 const rtLogin=express.Router()
 
-rtLogin.get('/ok',(req,res)=>{
-    res.json({response:'ok'})
-})
 
-rtLogin.get('/error',(req,res)=>{
-    res.json({response:'error'})
-})
+
+
 
 module.exports=rtLogin
--- routes/rtMain.js
+++ routes/rtMain.js
@@ -4,8 +4,15 @@ const daoUser=require('../dao/daoUser')
 //aqui te creas las rutas get, post, etc.. que necesies
 
 rtMain.get('/', function (req, res) {
+    res.json({response:'Server run!'})
+})
+
+rtMain.get('/ok',(req,res)=>{
     res.json({response:'ok'})
 })
 
+rtMain.get('/error',(req,res)=>{
+    res.json({response:'error'})
+})
 
 module.exports=rtMain
